f615727ae77ec5a480ab9ac666b180e89b23965f
dohuthyfa <dohuthayfa@gmail.com>
2020-09-27 11:15:22 +0300

    Bootstrap-

---
+++ Learn JavaScript by JavaScript.js
@@ -0,0 +1,30 @@
+ 
+var name = "string"; //verabile name as string 
+var number = 1;
+var boolean = true;
+var array = [1,2,3]; 
+var object = {x:1, y:2}
+
+typeof("") // string
+typeof (1) // number
+typeof (true) //boolean
+typeof [1,2,3]  // array
+typeof ({x:1, y:2}) // object
+// output 
+console.log()
+
+
+// Syntax Fixed and Variable values
+
+//1-Fixed values = Literals
+"Fixed values"
+'Fixed values'
+
+//2-Variable values = Variables.
+var x; 
+x=6; // number 
+
+var name;
+name= "Variable values" // string 
+
+
